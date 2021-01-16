e5a7f4ade1196f7decfaf2f68edf1a67eb46ad65
Spirit1489 <Spirit1489@gmail.com>
2021-01-16 19:26:19 +0300

    play sound

--- app/src/main/java/ru/spiritblog/mvvm_beat/Sound.kt
+++ app/src/main/java/ru/spiritblog/mvvm_beat/Sound.kt
@@ -3,7 +3,7 @@ package ru.spiritblog.mvvm_beat
 private const val WAV = ".wav"
 
 
-class Sound(val assetPath: String) {
+class Sound(val assetPath: String, var soundId: Int? = null) {
 
     val name = assetPath.split("/").last().removeSuffix(WAV)
 
